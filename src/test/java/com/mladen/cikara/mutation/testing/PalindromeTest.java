66c8370c57349ee0ecd96f4233f44baf2c0cef27
Mladen Čikara <mladen.cikara@gmail.com>
2018-06-25 20:07:05 +0200

    Solving problems with test

--- src/test/java/com/mladen/cikara/mutation/testing/PalindromeTest.java
+++ src/test/java/com/mladen/cikara/mutation/testing/PalindromeTest.java
@@ -1,5 +1,6 @@
 package com.mladen.cikara.mutation.testing;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,10 +8,23 @@ import org.junit.Test;
 public class PalindromeTest {
 
   @Test
+  public void whenNearPalindrom_thanReject() {
+    final Palindrome palindromeTester = new Palindrome();
+
+    assertFalse(palindromeTester.isPalindrome("neon"));
+  }
+
+  @Test
+  public void whenNotPalindrom_thanReject() {
+    final Palindrome palindromeTester = new Palindrome();
+
+    assertFalse(palindromeTester.isPalindrome("box"));
+  }
+
+  @Test
   public void whenPalindrom_thenAccept() {
     final Palindrome palindromeTester = new Palindrome();
 
     assertTrue(palindromeTester.isPalindrome("noon"));
   }
-
 }
